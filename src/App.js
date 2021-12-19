ab89b1b15b894edbaa44c8a298b5846abba59ddb
Rima SEMAAN <rimasemib@hotmail.com>
2021-12-19 16:44:49 +0100

    modified Notre Menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+
 import {useState} from "react";
 import Menu from "./components/Menu";
-import Categories from "./components/Categories";
+
 import items from "../src/assets/data";
 import './App.css';
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+
   return (
     <main>
       <section className="menu section">        
         <div className="title">
-          <h2>Our Menu</h2>
+          <h2>Notre Menu</h2>
           <div className="underLine"></div>
 
         </div>
-        <Categories />
+     
         <Menu items={menuItems} />      
       </section>
     </main>   
--- src/assets/data.js
+++ src/assets/data.js
@@ -4,7 +4,7 @@ const recettes = [
         title: "Hummos",
         category: 'entree',
         img: "https://st2.depositphotos.com/1373322/7398/i/950/depositphotos_73982225-stock-photo-classic-hummus-and-pita-bread.jpg",
-        desc: `Le houmous est une préparation culinaire du Proche-Orient, composée notamment de purée de pois chiches et de tahini (purée de sésame). Il s'agit d'un plat typique de la cuisine Libanaise.`,
+        desc: "Le houmous est une préparation culinaire du Proche-Orient, composée notamment de purée de pois chiches et de tahini (purée de sésame). Il s'agit d'un plat typique de la cuisine Libanaise.",
     },
 
       {
@@ -12,7 +12,7 @@ const recettes = [
         title: "Muhalabiya",
         category: 'dessert',
         img: "https://www.simplyleb.com/wp-content/uploads/milkcustard-7.jpg",
-        desc: `La recette libanaise du Mouhalabieh est un trésor de saveurs pour les amateurs de mélanges sucrés. Ce flan doux et tiède est préparé à base de lait, de maïzena et parfumé à la fleur d'oranger. Une bonne idée de crème pour voyager au moment du dessert !`,
+        desc: "La recette libanaise du Mouhalabieh est un trésor de saveurs pour les amateurs de mélanges sucrés. Ce flan doux et tiède est préparé à base de lait, de maïzena et parfumé à la fleur d'oranger. Une bonne idée de crème pour voyager au moment du dessert !",
     },
 
     {
@@ -20,7 +20,7 @@ const recettes = [
         title: "Koussa Mehchi (Courgettes Farcies Libanaises)",
         category: "plat",
         img: "https://i.pinimg.com/564x/70/f0/b8/70f0b8d52d9ed3c9c84607cd79dd0065.jpg",
-        desc: `Koussa Mehchi ! Cette recette nous vient du Moyen-Orient, ce plat familial réconfortant, très populaire au Liban. C'est une recette de courgettes farcies à la viande hachée et riz aux 7 épices libanaises, cuites dans une sauce rouge de tomate dans une marmite.`,
+        desc: "Koussa Mehchi ! Cette recette nous vient du Moyen-Orient, ce plat familial réconfortant, très populaire au Liban. C'est une recette de courgettes farcies à la viande hachée et riz aux 7 épices libanaises, cuites dans une sauce rouge de tomate dans une marmite.",
   
     },
    
@@ -29,7 +29,7 @@ const recettes = [
         title: "Aish Al Saraya",
         category: "dessert",
         img: " https://img.taste.com.au/HM6zc3ma/w643-h428-cfill-q90/taste/2016/11/sanioras-palace-bread-aish-el-saraya-107944-1.jpeg",
-        desc: `Ce flan libanais avec biscotte est crémeux et fondant en bouche, sans oublier la touche croquante.`,
+        desc: "Ce flan libanais avec biscotte est crémeux et fondant en bouche, sans oublier la touche croquante.",
   
     },
     {
@@ -37,7 +37,7 @@ const recettes = [
         title: "Taboulé",
         category: "plat",
         img: "https://www.mesinspirationsculinaires.com/wp-content/uploads/2016/03/taboule-libanais-1.jpg",
-        desc: `Le taboulé est une salade ou un mets froid à base de persil, de menthe et de boulghour (blé concassé), issue de la cuisine libanaise. Il est le plus souvent servi avec de la laitue romaine, mais peut aussi l'être avec des feuilles de chou blanc ou des feuilles de vigne fraîches.`,
+        desc: "Le taboulé est une salade ou un mets froid à base de persil, de menthe et de boulghour (blé concassé), issue de la cuisine libanaise. Il est le plus souvent servi avec de la laitue romaine, mais peut aussi l'être avec des feuilles de chou blanc ou des feuilles de vigne fraîches.",
   
     },
     {
@@ -45,7 +45,7 @@ const recettes = [
         title: "Le Loubieh",
         category: "plat",
         img: "https://assets.tmecosys.com/image/upload/t_web600x528/img/recipe/ras/Assets/68af4a2a-5755-455c-9918-0709aa12692f/Derivates/9068240a-f8ce-4a8e-a6d1-80c6e0fcffb0.jpg",
-        desc: `Des haricots verts plats en sauce qui se mangent froid ou chaud, avec ou sans riz, en fonction des envies.`,
+        desc: "Des haricots verts plats en sauce qui se mangent froid ou chaud, avec ou sans riz, en fonction des envies.",
  
     },
 

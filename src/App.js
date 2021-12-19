d5a70e302cb801a6971cdd706224085495ee4bdd
Rima SEMAAN <rimasemib@hotmail.com>
2021-12-19 14:50:19 +0100

    modified menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
+import {useState} from "react";
+import Menu from "./components/Menu";
+import Categories from "./components/Categories";
+import items from "../src/assets/data";
 import './App.css';
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
   return (
-    <div>
-      <h2>kjhjkjhjkkjj</h2>
+    <main>
+      <section className="menu section">        
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underLine"></div>
 
-    </div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />      
+      </section>
+    </main>   
   );
 }
 

35690d186f25b583c26c2c93b7612518eff92338
Rima SEMAAN <rimasemib@hotmail.com>
2021-12-22 14:52:22 +0100

    adding header to all pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Header from "../components/Header";
+
 
 const About = () => {
   return (
+    <div >
+    <Header />
     <div>
       <h2>about page</h2>
+    </div>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./Home.css";
+import Header from "../components/Header";
 
 
 
 const Home = () => {
   return (
+    <div >
+    <Header />
     <div>
       <h2>home page</h2>
+    </div>
     </div>
   )
 }

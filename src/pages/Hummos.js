49d0a49b4bd1324db3daf6ea78f378b97e9124a0
Rima SEMAAN <rimasemib@hotmail.com>
2021-12-29 10:36:24 +0100

    hummos final

--- src/pages/Hummos.js
+++ src/pages/Hummos.js
@@ -12,23 +12,26 @@ const Hummos = () => {
 
     <Header />
     <div className="myBody">
-	<h1 className="myHead">Pasta with Pesto and Tomatoes</h1>
+		<div className="myHead">
+			<h1 className="myHead">Houmous (purée de pois chiches)</h1>
+		</div>
 
 	<div className="myPic" >
 		<div className="imgContainer1" >
 	
-			<img className="myImg" src="https://assets.codepen.io/652/shania-pinnata-7E-vKgzahd8-unsplash.jpg" alt="spaghetti with pesto sauce and cherry tomatoes." />
+			<img className="myImg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Lebanese_style_hummus.jpg/1024px-Lebanese_style_hummus.jpg" alt="Houmous (purée de pois chiches)" />
 
 		</div>
 	</div>
-
-	
-		<p>This quick and delicious pasta dish is the perfect way to use up a summer bounty of basil and tomatoes! </p>
-		<p>Don't have a green thumb? Never fear! You can knock out this classic pasta dish in no time with a jar of prepared pesto sauce. </p>
+	<div className="myPar" >
+		<p>Houmous signifie pois-chiche en arabe. Il désigne donc uniquement la purée de pois chiche (à laquelle on mélange le tahini) : </p>
+		<p> aucun besoin donc de dire “houmous de pois chiche”.  </p>
+    <p>Le mot houmous a différentes orthographes en français : hoummous, homos, humus, houmos, oumos, hummus…</p>
+	</div>
 		<div className="myPic" >
 			<div className="imgContainer1" >
 	
-				<img className="myImg" src="https://assets.codepen.io/652/tamanna-rumee-nswz6tIpgZk-unsplash.jpg" alt="yummy"/>
+				<img className="myImg" src="https://healthyfitnessmeals.com/wp-content/uploads/2020/03/How-to-make-homemade-hummus-7.jpg" alt="purée de pois chiches"/>
 
 				</div>
 	</div>
@@ -36,50 +39,42 @@ const Hummos = () => {
 	
 
 	<div className="recipe">
-		<ul>
-			<li className="listy">Servings: 4</li>
-			<li className="listy">Prep Time: 5 minutes</li>
-			<li className="listy">Cook Time: 25 minutes</li>
-			<li className="listy">Total Time: 30 minutes</li>
+		<ul >
+			<li className="listy">Portions: 4 personnes</li>
+			<li className="listy">Temps de préparation:15 minutes</li>
+			<li className="listy">Temps de cuisson:15 minutes</li>
+			<li className="listy">Temps total:30 minutes</li>
 		</ul>
 	</div>
 	<div className="recipe">
-		<h2 className="ingredientsy">The Ingredients</h2>
+		<h2 className="ingredientsy">Les Ingrédients</h2>
 		<ul className="ingredients">
-			<li className="listy">8 oz. spaghetti or linguine pasta</li>
-			<li className="listy">8 oz. spaghetti or linguine pasta</li>
-			<li class="listy">2 cups fresh basil leaves, plus 4-8 reserved leaves for garnish</li>
-			<li claclassNamess="listy">2 cloves garlic</li>
-			<li className="listy">2 tbsp. pine nuts or blanched almonds</li>
-			<li className="listy">1/2 cup olive oil</li>
-			<li className="listy">1/2 cup grated Parmesan cheese, plus 2 tbsp. for garnish.</li>
-			<li className="listy">1/8 tsp salt</li>
-			<li className="listy">1 pint cherry tomatoes</li>
-			<li className="listy">1 tsp olive oil</li>
+			<li className="listy">200 g de pois chiches sec</li>
+			<li className="listy">100 g de Tahini(Crème de sesame)</li>
+			<li className="listy">1 citron(s)</li>
+			<li className="listy">2 gousses d' ail</li>
+			<li className="listy">2 pincées de bicarbonate de soude alimentaire</li>
+			<li className="listy">huile d'olive</li>
+			
 		</ul>
 
 	</div>
 	<div className="recipe">
-		<h2 className="processy">The Process</h2>
+		<h2 className="processy">Instructions</h2>
 		<ol className="instructions">
-			<li className="listy">Bring 4 quarts of water to a rolling boil in a large pot. Salt the water generously and add the pasta. </li>
-			<li className="listy">While the pasta cooks, make the pesto.
-				
-			</li>
-			<li className="listy">Heat a large skillet over medium heat. </li>
-			<li className="listy">Add the 1/2 tsp. olive oil to the heated skillet.</li>
-			<li className="listy">Add the cherry tomatoes to the skillet and let them blister, stirring occasionally and gently so they don't burst. </li>
-			<li className="listy">Remove tomatoes from the pan and set aside. </li>
-			<li className="listy">Drain the pasta, reserving 1/4 cup of the cooking water.</li>
-			<li className="listy">Return the pasta to the pot</li>
-			<li className="listy">Add the reserved cooking water and pesto to the pasta and stir to mix</li>
-			<li className="listy">Plate the pasta and add 6-8 tomatoes to each plate</li>
-			<li className="listy">Garnish with reserved basil leaves and Parmesean and serve. <strong>Buon Appetito!</strong></li>
+			<li className="listy">La veille - Préparer les pois chiches:Faire tremper les pois chiches toute la nuit dans 2 fois leur volume en eau. </li>
+			<li className="listy">Préparation du houmous:Verser les pois chiches dans une grande casserole. Recouvrir d'eau et ajouter 1 pincée de bicarbonate de soude. Faire cuire à petits bouillons pendant 30 minutes (les pois chiches doivent être tendres).Égoutter et réserver.</li>
+			<li className="listy">Peler l'ail et l'écraser. Mélanger avec le jus du citron </li>
+			<li className="listy">Mettre les pois chiche dans un mixeur. Ajouter l'ail écrasé et le citron, le tahiné ainsi qu'une pincée de bicarbonate de soude. Ajouter 2 . à soupe d'eau (idéalement l'eau de cuisson des pois chiche).Mixer jusqu'à obtenir une crème onctueuse, plus ou moins lisse (ça, c'est à vous de voir).</li>
+			<li className="listy">Déposer votre houmous dans l'assiette de service. Arroser d'huile d'olive et saupoudrer un peu de zaatar </li>
+			<li className="listy">Vous pouvez ajouter de l'huile d'olive à votre houmous lorsque vous le mixer. Cela va permettre de le rendre un peu plus liquide (mais il sera du coup plus gras). </li>
+			
 		</ol>
+		<p className="yummy"><strong >Sahtein!</strong></p>
 		<div className="myPic" >
 			<div className="imgContainer1" >
 	
-		<img src="https://www.nospoonnecessary.com/wp-content/uploads/2016/04/Arugula-Pesto-Pasta-671.jpg" alt='too'  />
+		<img src="https://cache.marieclaire.fr/data/photo/w1000_ci/4y/houmous-a-la-libanaise.webp" alt='Done'  />
 		</div>
 </div>
 	</div>

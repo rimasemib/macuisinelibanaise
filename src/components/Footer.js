daba35c7c4cd628b06228d8675e14262bd96287c
Rima SEMAAN <rimasemib@hotmail.com>
2021-12-29 17:54:13 +0100

    final footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Footer.css";
-import logo from "../assets/cuisineLogo.jpg";
 
 
 
 const Footer = () => {
     return (
-    
+
     <div className="footer">
         <div className="footer-left col-md-4 col-sm-6">
-            <div>
-                <span><i class="fab fa-facebook-square ginger"></i></span>
+        <div>
+
+				<span><i class="fab fa-facebook-square ginger"></i></span>
 				<span><i class="fab fa-instagram-square ginger"></i></span>
 				<span><i class="fab fa-twitter-square ginger"></i></span>
 
@@ -31,14 +31,12 @@ const Footer = () => {
             </div>
         </div>
         <div className="footer-right col-md-4 col-sm-6">
-            
-            <div className="imgContainer">
-                <img src= {logo} alt="My Logo" />
-            </div>
-            <p className="name1">La Cuisine Libanaise &copy; 2022</p>
+            <h2> La Cuisine Libanaise</h2>
+
+            <p className="name"> La Cuisine Libanaise &copy; 2022</p>
         </div>
     </div>
       );
     };
-    
+
     export default Footer;

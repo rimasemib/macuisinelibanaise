0d7df44231ca44648baa8810af6094f4cf7a8330
Rima SEMAAN <rimasemib@hotmail.com>
2022-01-02 17:44:26 +0100

    about us shadow

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,9 +8,10 @@ const About = () => {
   return (
     <div >
     <Header />
-    <div>
-      <h2>about page</h2>
-      <p>La cuisine libanaise ou la cuisine libanaise fait partie de la cuisine levantine répandue au Levant, dans les régions du nord de la Syrie et dans certaines régions de l'Irak.  ...la cuisine libanaise comprend une grande variété d'aliments, notamment des hors-d'œuvre, des grillades, des poêles à frire, des ragoûts et d'autres plats délicieux.  La cuisine libanaise est connue pour sa richesse en féculents, fruits, légumes et fruits de mer. Dans mon site que Denise cuisine et vous présente quelques plats de nos plats traditionnels libanais. Mélange de l'entrée plat principal et sucré car nous avons beaucoup de choses et vous présentons pour chaque plat.  Ce sont les ingrédients qui permettent de le faire de la meilleure manière et la plus simple, ainsi que les calories contenues dans chaque 100 g de ce plat avec le temps de préparation.  Combien de personnes combien de
+    <div className='boxes'>
+      <h2>About Us</h2>   
+      <p className='square shadow-5'>La cuisine libanaise ou la cuisine libanaise fait partie de la cuisine levantine répandue au Levant, dans les régions du nord de la Syrie et dans certaines régions de l'Irak.  ...la cuisine libanaise comprend
+       une grande variété d'aliments, notamment des hors-d'œuvre, des grillades, des poêles à frire, des ragoûts et d'autres plats délicieux.  La cuisine libanaise est connue pour sa richesse en féculents, fruits, légumes et fruits de mer. Dans mon site que Denise cuisine et vous présente quelques plats de nos plats traditionnels libanais. Mélange de l'entrée plat principal et sucré car nous avons beaucoup de choses et vous présentons pour chaque plat.  Ce sont les ingrédients qui permettent de le faire de la meilleure manière et la plus simple, ainsi que les calories contenues dans chaque 100 g de ce plat avec le temps de préparation.  Combien de personnes combien de
       
       Avec une présentation générale des commandes et des photos ci-dessus, certaines photos présentées le présentent de nombreuses manières et substitutions.
       Je souhaite que vous aimerez mon site Web et m'encouragez à ajouter de plus en plus de détails et l'international je souhaite qu'il vous plaira et vous apprécierez nos repas.</p>
